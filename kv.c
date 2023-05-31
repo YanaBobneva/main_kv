8cdd3c8a1e19aca1e7f8ee8b6c5538cb409cf6f7
Yana <bobneva.y@mail.ru>
2023-05-31 22:12:07 +0600

    raznost

--- kv.c
+++ kv.c
@@ -4,7 +4,9 @@
         int a = 5;
         int b = 3;
         int sum = a + b;
+        int difference = a - b;
         printf("Сумма: %d\n", sum);
+        printf("Разность: %d\n", difference);
         return 0;
     }
 
